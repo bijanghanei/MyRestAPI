a2b0c7c92cdfd19b507b8c827cafaf00011697ad
bijanghanei <iambijanghanei@gmail.com>
2023-11-24 17:47:52 -0500

    spring data rest added

--- MyRestAPI/src/main/java/com/bijanghanei/MyRestAPI/service/EmployeeService.java
+++
@@ -1,9 +0,0 @@
-package com.bijanghanei.MyRestAPI.service;
-
-import com.bijanghanei.MyRestAPI.entity.Employee;
-
-import java.util.List;
-
-public interface EmployeeService {
-    List<Employee> getAllEmployees();
-}
